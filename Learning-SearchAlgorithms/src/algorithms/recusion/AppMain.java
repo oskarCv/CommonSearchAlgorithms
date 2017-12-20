bb844810908dcd9a32a32248dd4141a8a1f78f06
Oskar <oskar.cv91@gmail.com>
2017-12-20 16:40:36 -0600

    updating file
    works with the recursion linear search

--- Learning-SearchAlgorithms/src/algorithms/recusion/AppMain.java
+++ Learning-SearchAlgorithms/src/algorithms/recusion/AppMain.java
@@ -3,8 +3,12 @@ package algorithms.recusion;
 public class AppMain {
 
 	public static void main(String[] args) {
-		Recursion recursion = new Recursion();
-		recursion.incrementByOne(0);
+		/*Recursion recursion = new Recursion();
+		recursion.incrementByOne(0);*/
+		RecursiveLinearSearch linearSearch = new RecursiveLinearSearch();
+		int a[]={1,5,6,8,9,10,25,11};
+		
+		linearSearch.linearSearch(a, 13, a.length-1);
 	}
 
 }
